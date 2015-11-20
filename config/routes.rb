ecd9677577d658369c4fda4ea415ed5d1f4e4d7d
Mingsheng Xie <mingsheng.xie@gmail.com>
2015-11-19 21:54:42 -0500

    Uploaded attachments are now served through the AttachmentsController for extra security

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :attachments, only: [:show]
 
   # Example resource route with options:
   #   resources :products do
