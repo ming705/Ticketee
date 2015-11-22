8c72e3908f66b2ef514120bca376d69c2695d746
Mingsheng Xie <mingsheng.xie@gmail.com>
2015-11-22 18:05:23 -0500

    Add the ability for users to remove tags from tickets

--- config/routes.rb
+++ config/routes.rb
@@ -57,6 +57,11 @@ Rails.application.routes.draw do
   
   resources :tickets, only: [] do 
     resources :comments, only: [:create]
+    resources :tags, only: [] do 
+      member do
+        delete :remove
+      end
+    end
   end
 
   # Example resource route with more complex sub-resources:
